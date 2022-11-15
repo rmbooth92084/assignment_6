4e482df3407b4058ab80a4c9c1ce71f1d2df6c5c
Raymond Booth <rmbooth92084@gmail.com>
2022-11-15 12:31:21 -0800

    Initial Commit

---
+++ assignment_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment_6
+//
+//  Created by user227341 on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
