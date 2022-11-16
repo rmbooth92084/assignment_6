53f30f835888c02b19dc326f96da076e9d15a24b
Raymond Booth <rmbooth92084@gmail.com>
2022-11-16 14:21:56 -0800

    have most fields in rough places and implementation of success and failure to send. Had birth date picker

--- assignment_6/ViewController.swift
+++ assignment_6/ViewController.swift
@@ -7,13 +7,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var dateInput: UIDatePicker!
+    
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var successLabel: UILabel!
+    var valid : [Bool] = []
+    
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Signup Sheet"
+        
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        dateTextField.inputView = dateInput
+        dateInput.datePickerMode = .date
+        dateTextField.text = dateFormatter.string(from: dateInput.date)
+        
+        lastNameTextField.delegate = self
+        firstNameTextField.delegate = self
+        
+        
+        validSetup()
+    }
+    
+    func validSetup(){
+        var num = 6
+        
+        for i in 1...num{
+            valid.append(false)
+        }
+        
+    }
+    func checkFields(){
+        valid[0] = lastNameTextField.text!.isEmpty ? false : true
+        valid[1] = firstNameTextField.text!.isEmpty ? false : true
+        valid[2] = zipTextField.text!.isEmpty ? false : true
+        valid[3] = cityTextField.text!.isEmpty ? false : true
+        valid[4] = streetTextField.text!.isEmpty ? false : true
+        valid[5] = emailTextField.text!.isEmpty ? false : true
+        
     }
 
-
+    @IBAction func datePickerValueChanged(_ sender: Any) {
+        dateTextField.text = dateFormatter.string(from: dateInput.date)
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        print("Button pressed")
+        checkFields()
+        //print(lastNameTextField.text)
+        var isSuccess = true
+        for i in valid{
+            print("Item: \(i)")
+            if(!i){
+                isSuccess = false
+            }
+        }
+        print(isSuccess)
+        if(isSuccess){
+            successLabel.isHidden = false
+        }
+        
+        
+    }
 }
 
